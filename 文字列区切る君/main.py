46b9219e24d26a0379ca55279dc27ad331f92a05
erudo <hashierudo@gmail.com>
2018-12-28 16:14:58 +0900

    Add files via upload

---
+++ 文字列区切る君/main.py
@@ -0,0 +1,51 @@
+#coding: utf-8
+
+import sys,io
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import QIcon
+
+class Main(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.mainUI()
+    
+    def mainUI(self):
+        self.setGeometry(300,300,300,300)
+        self.setWindowTitle('文字区切り君')
+        
+        self.label = QLabel('文字列')
+        self.lineEdit = QLineEdit(self)
+        self.button = QPushButton('区切る！',self)
+        self.button.clicked.connect(self.buttonClicked)
+
+        #layout
+        self.vbox = QVBoxLayout()
+        self.vbox.addWidget(self.lineEdit)
+        self.vbox.addWidget(self.button)
+        
+
+        self.setLayout(self.vbox)
+        self.show()
+    
+    def buttonClicked(self):
+        if(self.lineEdit.text() is None):
+            pass
+        contents = str(self.lineEdit.text())
+        chars = list(contents)
+        strings = '/'.join(chars)
+
+        f = open('textfile.txt', 'w')
+        f.write(strings)
+        f.close()
+
+        
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    gui = Main()
+    sys.exit(app.exec_())
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
